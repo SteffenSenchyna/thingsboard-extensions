86415c79067f75115a0ec7cf9065f56168224148
steffen.senchyna <steffen.senchyna@lafarge.com>
2025-04-09 22:20:16 -0600

    Refactor HTML structure and update resource URLs in JSON files

--- builders/static-serve/index.ts
+++ builders/static-serve/index.ts
@@ -2,21 +2,26 @@
 /// Copyright © 2023 ThingsBoard, Inc.
 ///
 
-import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
-import * as express from 'express';
-import * as http from 'http';
-import { NgPackagrBuilderOptions } from '@angular-devkit/build-angular';
-import { resolve } from 'path';
-import { from, Observable } from 'rxjs';
-import { mapTo, switchMap, tap } from 'rxjs/operators';
-import { NgPackagr, ngPackagr } from 'ng-packagr';
-import { existsSync } from 'fs';
-import { watch } from 'chokidar';
-import { execSync } from 'child_process';
+import {
+  BuilderContext,
+  BuilderOutput,
+  createBuilder,
+} from "@angular-devkit/architect";
+import * as express from "express";
+import * as http from "http";
+import { NgPackagrBuilderOptions } from "@angular-devkit/build-angular";
+import { resolve } from "path";
+import { from, Observable } from "rxjs";
+import { mapTo, switchMap, tap } from "rxjs/operators";
+import { NgPackagr, ngPackagr } from "ng-packagr";
+import { existsSync } from "fs";
+import { watch } from "chokidar";
+import { execSync } from "child_process";
 
 interface StaticServeOptions extends NgPackagrBuilderOptions {
   staticServeConfig: string;
   port: number;
+  host?: string;
 }
 
 interface StaticServeConfig {
@@ -29,7 +34,7 @@ let server: http.Server = null;
 
 async function initialize(
   options: StaticServeOptions,
-  root: string,
+  root: string
 ): Promise<NgPackagr> {
   const packager = ngPackagr();
 
@@ -42,26 +47,44 @@ async function initialize(
   return packager;
 }
 
-
-function watchStyles(options: StaticServeOptions,
-                     context: BuilderContext) {
-  const styleScss = resolve(context.workspaceRoot, 'src', 'app', 'scss', 'style.scss');
+function watchStyles(options: StaticServeOptions, context: BuilderContext) {
+  const styleScss = resolve(
+    context.workspaceRoot,
+    "src",
+    "app",
+    "scss",
+    "style.scss"
+  );
   if (existsSync(styleScss)) {
-    const styleCompScss = resolve(context.workspaceRoot, 'src', 'app', 'scss', 'style.comp.scss');
+    const styleCompScss = resolve(
+      context.workspaceRoot,
+      "src",
+      "app",
+      "scss",
+      "style.comp.scss"
+    );
     context.logger.info(`==> Watching library styles: ${styleScss}`);
-    const postcss = resolve(context.workspaceRoot, 'node_modules', '.bin', 'postcss');
-    watch(styleScss).on('change', () => {
+    const postcss = resolve(
+      context.workspaceRoot,
+      "node_modules",
+      ".bin",
+      "postcss"
+    );
+    watch(styleScss).on("change", () => {
       const compileStyleScssCommand = `${postcss} ${styleScss} -o ${styleCompScss}`;
-      executeCliCommand(context, compileStyleScssCommand, 'Compile style.scss')
+      executeCliCommand(context, compileStyleScssCommand, "Compile style.scss");
     });
   }
 }
 
-function executeCliCommand(context: BuilderContext,
-                           cliCommand: string, description: string) {
+function executeCliCommand(
+  context: BuilderContext,
+  cliCommand: string,
+  description: string
+) {
   try {
     execSync(cliCommand, {
-      stdio: 'inherit'
+      stdio: "inherit",
     });
   } catch (err) {
     context.logger.error(`==> ${description} failed`, err);
@@ -71,35 +94,44 @@ function executeCliCommand(context: BuilderContext,
 
 export function execute(
   options: StaticServeOptions,
-  context: BuilderContext,
+  context: BuilderContext
 ): Observable<BuilderOutput> {
   watchStyles(options, context);
   return from(initialize(options, context.workspaceRoot)).pipe(
-    switchMap(packager => {
+    switchMap((packager) => {
       return packager.watch().pipe(
         tap(() => {
           createServer(options, context);
         })
       );
     }),
-    mapTo({ success: true }),
+    mapTo({ success: true })
   );
 }
 
-export function createServer(options: StaticServeOptions, context: BuilderContext) {
+export function createServer(
+  options: StaticServeOptions,
+  context: BuilderContext
+) {
   if (server) {
     server.close();
     server = null;
   }
   const app = express();
 
-  const staticServeConfig: StaticServeConfig = require(resolve(context.workspaceRoot, options.staticServeConfig));
+  const staticServeConfig: StaticServeConfig = require(resolve(
+    context.workspaceRoot,
+    options.staticServeConfig
+  ));
   for (const path of Object.keys(staticServeConfig)) {
     const route = staticServeConfig[path];
     app.get(path, (req, res) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-      if (path.endsWith('*')) {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "origin, content-type, accept"
+      );
+      if (path.endsWith("*")) {
         const target = req.params[0];
         res.sendFile(resolve(context.workspaceRoot, route.target + target));
       } else {
@@ -116,13 +148,17 @@ export function createServer(options: StaticServeOptions, context: BuilderContex
     }); */
 
   server = http.createServer(app);
-  const host = 'localhost';
-  server.on('error', (error) => {
+  const host = options.host || "localhost";
+  server.on("error", (error) => {
     context.logger.error(error.message);
   });
   server.listen(options.port, host, 511, () => {
-    context.logger.info(`==> 🌎  Listening on port ${options.port}. Open up http://localhost:${options.port}/ in your browser.`);
+    context.logger.info(
+      `==> 🌎  Listening on port ${options.port} at host ${host}. Open up http://${host}:${options.port}/ in your browser.`
+    );
   });
 }
 
-export default createBuilder<Record<string, string> & StaticServeOptions>(execute);
+export default createBuilder<Record<string, string> & StaticServeOptions>(
+  execute
+);
--- install.js
+++ install.js
@@ -1,16 +1,37 @@
 /*
  * Copyright © 2023 ThingsBoard, Inc.
  */
-const fse = require('fs-extra');
-const path = require('path');
+const fse = require("fs-extra");
+const path = require("path");
 
 let _projectRoot = null;
 
-(async() => {
-  await fse.move(sourcePackage(),
-    targetPackage(),
-    {overwrite: true});
+(async () => {
+  // Move the JavaScript file and its associated source map
+  await moveFileWithSourceMap(sourcePackage(), targetPackage());
 })();
+
+// Function to move the main package and its source map
+async function moveFileWithSourceMap(sourceFilePath, targetFilePath) {
+  try {
+    // Move the main JavaScript file
+    await fse.move(sourceFilePath, targetFilePath, { overwrite: true });
+    console.log(`Moved: ${sourceFilePath} -> ${targetFilePath}`);
+
+    // Check if a source map exists and move it if found
+    const sourceMapPath = `${sourceFilePath}.map`;
+    const targetMapPath = `${targetFilePath}.map`;
+
+    if (fse.pathExists(sourceMapPath)) {
+      await fse.move(sourceMapPath, targetMapPath, { overwrite: true });
+      console.log(`Moved: ${sourceMapPath} -> ${targetMapPath}`);
+    } else {
+      console.log(`Source map not found for: ${sourceFilePath}`);
+    }
+  } catch (err) {
+    console.error(`Error moving files: ${err.message}`);
+  }
+}
 
 function projectRoot() {
   if (!_projectRoot) {
@@ -20,9 +41,20 @@ function projectRoot() {
 }
 
 function sourcePackage() {
-  return path.join(projectRoot(), 'dist', 'widget-extension', 'system', 'thingsboard-extension-widgets.js');
+  return path.join(
+    projectRoot(),
+    "dist",
+    "widget-extension",
+    "system",
+    "thingsboard-extension-widgets.js"
+  );
 }
 
 function targetPackage() {
-  return path.join(projectRoot(), 'target', 'generated-resources', 'thingsboard-extension-widgets.js');
+  return path.join(
+    projectRoot(),
+    "target",
+    "generated-resources",
+    "thingsboard-extension-widgets.js"
+  );
 }
