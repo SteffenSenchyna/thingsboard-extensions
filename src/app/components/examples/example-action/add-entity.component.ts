4413127f7d07e946e6a23a6d63b79dd7821d5dee
steffen.senchyna <steffen.senchyna@lafarge.com>
2025-09-22 21:09:32 -0600

    UI: Refactor add entity form layout and improve code consistency

--- src/app/components/examples/example-action/add-entity.component.ts
+++ src/app/components/examples/example-action/add-entity.component.ts
@@ -1,225 +1,164 @@
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
-import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
   AttributeScope,
   EntitySearchDirection,
   EntityType,
   PageComponent,
-  RelationTypeGroup,
-} from "@shared/public-api";
-import { AppState } from "@core/core.state";
-import { Store } from "@ngrx/store";
-import { WidgetContext } from "@home/models/widget-component.models";
-import { DialogRef } from "@angular/cdk/dialog";
-import {
-  AssetService,
-  AttributeService,
-  DeviceService,
-  EntityRelationService,
-} from "@core/public-api";
-import { Asset } from "@shared/models/asset.models";
-import { Device } from "@shared/models/device.models";
-import { forkJoin, mergeMap, Observable, of, Subject, takeUntil } from "rxjs";
-import { EntityId } from "@shared/models/id/entity-id";
-import { EntityRelation } from "@shared/models/relation.models";
+  RelationTypeGroup
+} from '@shared/public-api';
+import { AppState } from '@core/core.state';
+import { Store } from '@ngrx/store';
+import { WidgetContext } from '@home/models/widget-component.models';
+import { DialogRef } from '@angular/cdk/dialog';
+import { AssetService, AttributeService, DeviceService, EntityRelationService } from '@core/public-api';
+import { Asset } from '@shared/models/asset.models';
+import { Device } from '@shared/models/device.models';
+import { forkJoin, mergeMap, Observable, of, Subject, takeUntil } from 'rxjs';
+import { EntityId } from '@shared/models/id/entity-id';
+import { EntityRelation } from '@shared/models/relation.models';
 
 @Component({
-  selector: "tb-add-entity-action",
-  templateUrl: "./add-entity.component.html",
-  styleUrls: ["./add-entity.component.scss"],
+    selector: 'tb-add-entity-action',
+    templateUrl: './add-entity.component.html',
+    styleUrls: ['./add-entity.component.scss']
 })
-export class AddEntityComponent
-  extends PageComponent
-  implements OnInit, OnDestroy
-{
-  @Input() ctx: WidgetContext;
 
-  @Input() dialogRef: DialogRef;
-  public addEntityFormGroup: FormGroup;
-  public allowedEntityTypes: EntityType[] = [
-    EntityType.ASSET,
-    EntityType.DEVICE,
-  ];
-  public readonly entityType = EntityType;
-  public readonly entitySearchDirection = EntitySearchDirection;
-  private destroy$ = new Subject<void>();
+export class AddEntityComponent extends PageComponent implements OnInit, OnDestroy {
 
-  constructor(
-    protected store: Store<AppState>,
-    private fb: FormBuilder,
-    private deviceService: DeviceService,
-    private assetService: AssetService,
-    private attributeService: AttributeService,
-    private entityRelationService: EntityRelationService
-  ) {
-    super(store);
-  }
+    @Input() ctx: WidgetContext;
 
-  ngOnInit(): void {
-    this.addEntityFormGroup = this.fb.group({
-      entityName: ["", [Validators.required]],
-      entityType: [EntityType.DEVICE],
-      entityLabel: [null],
-      type: ["", [Validators.required]],
-      attributes: this.fb.group({
-        latitude: [null],
-        longitude: [null],
-        address: [null],
-        owner: [null],
-        number: [null, [Validators.pattern(/^-?[0-9]+$/)]],
-        booleanValue: [null],
-      }),
-      relations: this.fb.array([]),
-    });
-  }
+    @Input() dialogRef: DialogRef;
+    public addEntityFormGroup: FormGroup;
+    public allowedEntityTypes: EntityType[] = [EntityType.ASSET, EntityType.DEVICE];
+    public readonly entityType = EntityType;
+    public readonly entitySearchDirection = EntitySearchDirection;
+    private destroy$ = new Subject<void>();
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+    constructor(
+        protected store: Store<AppState>,
+        private fb: FormBuilder,
+        private deviceService: DeviceService,
+        private assetService: AssetService,
+        private attributeService: AttributeService,
+        private entityRelationService: EntityRelationService
+    ) {
+        super(store);
+    }
+    ngOnInit(): void {
+        this.addEntityFormGroup = this.fb.group({
+            entityName: ['', [Validators.required]],
+            entityType: [EntityType.DEVICE],
+            entityLabel: [null],
+            type: ['', [Validators.required]],
+            attributes: this.fb.group({
+                latitude: [null],
+                longitude: [null],
+                address: [null],
+                owner: [null],
+                number: [null, [Validators.pattern(/^-?[0-9]+$/)]],
+                booleanValue: [null]
+            }),
+            relations: this.fb.array([])
+        });
+    }
 
-  public relations(): FormArray {
-    return this.addEntityFormGroup.get("relations") as FormArray;
-  }
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 
-  public removeRelation(index: number): void {
-    this.relations().removeAt(index);
-    this.relations().markAsDirty();
-  }
+    public relations(): FormArray {
+        return this.addEntityFormGroup.get('relations') as FormArray;
+    }
 
-  public addRelation(): void {
-    this.relations().push(
-      this.fb.group({
-        relatedEntity: [null, [Validators.required]],
-        relationType: [null, [Validators.required]],
-        direction: [null, [Validators.required]],
-      })
-    );
-  }
+    public removeRelation(index: number): void {
+        this.relations().removeAt(index);
+        this.relations().markAsDirty();
+    }
 
-  public save(): void {
-    this.addEntityFormGroup.markAsPristine();
-    this.saveEntityObservable()
-      .pipe(
-        mergeMap((entity: Asset | Device) =>
-          forkJoin([
-            this.saveAttributes(entity.id),
-            this.saveRelations(entity.id),
-          ])
-        ),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(() => {
-        this.ctx.updateAliases();
-        this.dialogRef.close(null);
-      });
-  }
-
-  public cancel(): void {
-    this.dialogRef.close(null);
-  }
-
-  private saveEntityObservable(): Observable<Asset | Device> {
-    const formValues = this.addEntityFormGroup.value;
-    const entity = {
-      name: formValues.entityName,
-      type: formValues.type,
-      label: formValues.entityLabel,
+    public addRelation(): void {
+        this.relations().push(
+            this.fb.group({
+                relatedEntity: [null, [Validators.required]],
+                relationType: [null, [Validators.required]],
+                direction: [null, [Validators.required]]
+            })
+        );
     };
-    if (formValues.entityType === EntityType.ASSET) {
-      return this.assetService.saveAsset(entity);
-    } else if (formValues.entityType === EntityType.DEVICE) {
-      return this.deviceService.saveDevice(entity);
-    }
-  }
 
-  private saveRelations(entityId: EntityId): Observable<EntityRelation[]> {
-    const relations = this.addEntityFormGroup.get("relations").value;
-    const tasks: Observable<EntityRelation>[] = [];
-    for (const newRelation of relations) {
-      const relation: EntityRelation = {
-        type: newRelation.relationType,
-        typeGroup: RelationTypeGroup.COMMON,
-        to: null,
-        from: null,
-      };
-
-      if (newRelation.direction === EntitySearchDirection.FROM) {
-        relation.to = newRelation.relatedEntity;
-        relation.from = entityId;
-      } else {
-        relation.to = entityId;
-        relation.from = newRelation.relatedEntity;
-      }
-      tasks.push(this.entityRelationService.saveRelation(relation));
+    public save(): void {
+        this.addEntityFormGroup.markAsPristine();
+        this.saveEntityObservable().pipe(
+            mergeMap((entity: Asset | Device) => forkJoin([
+                    this.saveAttributes(entity.id),
+                    this.saveRelations(entity.id)
+                ])
+            ),
+            takeUntil(this.destroy$)
+        ).subscribe(() => {
+                this.ctx.updateAliases();
+                this.dialogRef.close(null);
+            }
+        );
     }
 
-    if (tasks.length > 0) {
-      return forkJoin(tasks);
+    public cancel(): void {
+        this.dialogRef.close(null);
     }
-    return of([]);
-  }
 
-  private saveAttributes(entityId: EntityId): Observable<any> {
-    const attributes = this.addEntityFormGroup.get("attributes").value;
-    const attributesArray = [];
-    for (const key in attributes) {
-      if (attributes[key] !== null) {
-        attributesArray.push({ key, value: attributes[key] });
-      }
+    private saveEntityObservable(): Observable<Asset | Device> {
+        const formValues = this.addEntityFormGroup.value;
+        const entity= {
+            name: formValues.entityName,
+            type: formValues.type,
+            label: formValues.entityLabel
+        };
+        if (formValues.entityType === EntityType.ASSET) {
+            return this.assetService.saveAsset(entity);
+        } else if (formValues.entityType === EntityType.DEVICE) {
+            return this.deviceService.saveDevice(entity);
+        }
     }
-    if (attributesArray.length > 0) {
-      return this.attributeService.saveEntityAttributes(
-        entityId,
-        AttributeScope.SERVER_SCOPE,
-        attributesArray
-      );
-    }
-    return of([]);
-  }
 
-  // private getOrCreateAssetGroup(ownerId: EntityId) {
-  //   return this.getEntityGroupByName(
-  //     ownerId,
-  //     "QC Assets",
-  //     EntityType.ASSET
-  //   ).pipe(
-  //     this.ctx.rxjs.switchMap((group) => {
-  //       if (group) {
-  //         return this.ctx.rxjs.of(group);
-  //       } else {
-  //         const entityGroup = {
-  //           type: EntityType.ASSET,
-  //           name: "QC Assets",
-  //           ownerId: ownerId,
-  //         };
-  //         return this.entityGroupService.saveEntityGroup(entityGroup);
-  //       }
-  //     })
-  //   );
-  // }
-  //
-  // private getEntityGroupByName(
-  //   ownerId: EntityId,
-  //   groupName: string,
-  //   groupType: EntityType
-  // ) {
-  //   const pageLink = this.ctx.pageLink(10, 0, groupName);
-  //   return this.entityGroupService
-  //     .getEntityGroupsByOwnerIdAndPageLink(
-  //       ownerId.entityType,
-  //       ownerId.id,
-  //       groupType,
-  //       pageLink,
-  //       { ignoreLoading: true }
-  //     )
-  //     .pipe(
-  //       this.ctx.rxjs.map((data) => {
-  //         return data.data.length
-  //           ? // @ts-ignore
-  //             data.data.find((g) => g.name === groupName)
-  //           : null;
-  //       })
-  //     );
-  // }
+    private saveAttributes(entityId: EntityId): Observable<any> {
+        const attributes = this.addEntityFormGroup.get('attributes').value;
+        const attributesArray = [];
+        for (const key in attributes) {
+            if(attributes[key] !== null) {
+                attributesArray.push({key, value: attributes[key]});
+            }
+        }
+        if (attributesArray.length > 0) {
+            return this.attributeService.saveEntityAttributes(entityId, AttributeScope.SERVER_SCOPE, attributesArray);
+        }
+        return of([]);
+    }
+
+    private saveRelations(entityId: EntityId): Observable<EntityRelation[]> {
+        const relations = this.addEntityFormGroup.get('relations').value;
+        const tasks: Observable<EntityRelation>[] = [];
+        for (const newRelation of relations) {
+            const relation: EntityRelation = {
+                type: newRelation.relationType,
+                typeGroup: RelationTypeGroup.COMMON,
+                to: null,
+                from: null
+            };
+
+            if (newRelation.direction === EntitySearchDirection.FROM) {
+                relation.to = newRelation.relatedEntity;
+                relation.from = entityId;
+            } else {
+                relation.to = entityId;
+                relation.from = newRelation.relatedEntity;
+            }
+            tasks.push(this.entityRelationService.saveRelation(relation));
+        }
+
+        if (tasks.length > 0) {
+            return forkJoin(tasks);
+        }
+        return of([]);
+    }
 }
